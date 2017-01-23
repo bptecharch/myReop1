b738f4300bd6404c5580d44b3f146c0ff3ca4887
bptech <bptecharch@gmail.com>
2017-01-22 23:28:36 -0500

    new py code for publishing

---
+++ publisher.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+my_records = [
+       {"type": "Sales", "value": "house_for_sale"},
+        {"type": "Lease", "value": "apt_for_lease"},
+        {"type": "ShortSales", "value": "house_short_sale"},
+        {"type": "TempRent", "value": "temp_rent"}
+]
+
+for record in my_records:
+	try:
+		requests.post("http://127.0.0.1:5000/bptech/project/streams", json = record)
+	except Exception as e:
+		print("Exception thrown when post to REST api: %s", e)
